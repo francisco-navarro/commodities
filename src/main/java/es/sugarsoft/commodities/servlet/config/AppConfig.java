be0e0ea380c1663589bbb230478988fe93a69822
packarbell@gmail.com <packarbell@gmail.com>
2015-09-17 21:09:21 +0200

    quitar import de test

--- src/main/java/es/sugarsoft/commodities/servlet/config/AppConfig.java
+++ src/main/java/es/sugarsoft/commodities/servlet/config/AppConfig.java
@@ -12,9 +12,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -23,8 +20,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import es.sugarsoft.commodities.resources.persistence.MyMapper;
-import es.sugarsoft.test.support.appconfig.DataSourceConfig;
-import es.sugarsoft.test.support.appconfig.DataSourceConfig;
 
 @Configuration
 @ComponentScan({
