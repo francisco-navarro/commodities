3e39668281a9423f20570fba15b372deeddd286b
Paco Navarro <packarbell@gmail.com>
2016-12-28 19:48:26 +0100

    Init config for angular 2

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var server = require('./server/index');
-var bodyParser = require('body-parser');
-
 var app = express();
 var appPort = process.env.PORT || 3000;
 
 server.init(app);
 
-app.use('/', express.static(__dirname + '/src/'));
+app.use('/', express.static(__dirname + '/client/'));
 app.use('/lib', express.static(__dirname + '/node_modules/'));
 app.use('/img', express.static(__dirname + '/img/'));
 
