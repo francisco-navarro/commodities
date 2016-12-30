138a5deea10209de51e162b7334e7de30cb98c1f
Paco Navarro <packarbell@gmail.com>
2016-12-30 23:31:00 +0100

    add

--- client/app/services/currency.service.ts
+++ client/app/services/currency.service.ts
@@ -11,7 +11,8 @@ export class CurrencyService {
   constructor(private http: Http) { }
 
   getCurrencies(): Promise<Currency[]> {
-    return this.http.get('http://openexchangerates.org/api/currencies.json').toPromise()
+    return this.http.get('http://openexchangerates.org/api/currencies.json')
+      .toPromise()
       .then(response =>
         Object.keys(response.json()).reduce((arr, next) => {
           arr.push({
