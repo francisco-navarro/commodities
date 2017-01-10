48946bc00ad6f296a0c5c05e1b6a63ca343de440
Paco Navarro <packarbell@gmail.com>
2017-01-10 19:12:00 +0100

    add

--- client/app/services/currency.service.ts
+++ client/app/services/currency.service.ts
@@ -24,7 +24,7 @@ export class CurrencyService {
   }
   getCurrency(id: string){
     var url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USD${id}%22%2C%20%22USD${id}%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`;
-    this.http.get('http://openexchangerates.org/api/currencies.json').toPromise()
+    this.http.get(url).toPromise()
       .then(response => response.query.results.rate);
   }
 }
