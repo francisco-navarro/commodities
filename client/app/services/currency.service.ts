b4a972c57f2994bb42fa79f2075b4292df335901
Paco Navarro <packarbell@gmail.com>
2017-01-10 19:10:00 +0100

    Currency value

--- client/app/services/currency.service.ts
+++ client/app/services/currency.service.ts
@@ -21,6 +21,10 @@ export class CurrencyService {
           return arr;
         }, []) as Currency[]
       );
-      //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDAED%22%2C%20%22USDCHF%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+  }
+  getCurrency(id: string){
+    var url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USD${id}%22%2C%20%22USD${id}%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`;
+    this.http.get('http://openexchangerates.org/api/currencies.json').toPromise()
+      .then(response => response.query.results.rate);
   }
 }
